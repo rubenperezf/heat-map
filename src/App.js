a46bed127520d2d636673a34680503c8d0443796
rubenperezf <rubenperezfeijoo@gmail.com>
2021-02-18 19:19:35 -0500

    add florence to the map

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { geoAlbers, geoAlbersUsa, geoCentroid } from "d3-geo";
+// import * as d3 from "d3";
+// import * as topojson from "topojson";
+import simpleheat from "./simpleheat.js";
+import latLong from "./latlong.js";
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  // Marker,
+  // Annotation,
+} from "react-simple-maps";
+import { useEffect } from "react";
+
+let data = [];
+
+const projection = geoAlbersUsa()
+  .scale(800)
+  .translate([800 / 2, 450 / 2]);
 
 function App() {
+  useEffect(() => {
+    const elem = document.getElementById("heatmap");
+    if (elem) {
+      let canvas = elem.getContext("2d");
+      latLong.map((x) => data.push(projection([x[0], x[1]])));
+
+      simpleheat("heatmap").radius(5, 5).max(100).data(data).draw();
+    }
+  }, [document.getElementById("heatmap")]);
+  const geoUrl = "https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ position: "relative", width: "800px", height: "450px" }}>
+      <canvas
+        id="heatmap"
+        width="800px"
+        height="450px"
+        style={{
+          position: "absolute",
+          left: "0",
+          opacity: 0.5,
+        }}
+      ></canvas>
+      <ComposableMap
+        style={{
+          display: "block",
+          zIndex: 1,
+          opacity: 1,
+          width: "800px",
+          height: "450px",
+        }}
+        projection="geoAlbersUsa"
+      >
+        <Geographies geography={geoUrl}>
+          {({ geographies }) => (
+            <>
+              {geographies.map((geo) => (
+                <Geography
+                  key={geo.rsmKey}
+                  stroke="#FFF"
+                  geography={geo}
+                  fill="#DDD"
+                />
+              ))}
+            </>
+          )}
+        </Geographies>
+      </ComposableMap>
     </div>
   );
 }
